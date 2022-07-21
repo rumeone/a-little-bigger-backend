e160e7c9b35ad23d2724a14e02aaa9f8d2286d6f
rumeone <yuaroslav29@gmail.com>
2022-07-22 01:58:05 +0700

    added registartion users

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {CreateUserDto} from "../users/dto/create.user.dto";
+import {ApiTags} from "@nestjs/swagger";
+import {AuthService} from "./auth.service";
+
+@ApiTags("Авторизация")
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto);
+    }
+
+    @Post('/registration')
+    registration(@Body() userDto: CreateUserDto) {
+        return this.authService.registration(userDto);
+    }
+
+}
