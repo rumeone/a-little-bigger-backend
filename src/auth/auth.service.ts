398c86f1a46b00ba9516855670b79f6493d30edf
rumeone <yuaroslav29@gmail.com>
2022-07-22 02:08:56 +0700

    added login users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import {Body, HttpException, HttpStatus, Injectable, Post} from '@nestjs/common';
+import {Body, HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
 import {CreateUserDto} from "../users/dto/create.user.dto";
 import {UsersService} from "../users/users.service";
 import {JwtService} from "@nestjs/jwt";
@@ -12,8 +12,9 @@ export class AuthService {
     }
 
    async login(userDto: CreateUserDto) {
+        const user = await this.validateUser(userDto);
+        return this.generateToken(user);
     }
-
     async registration(userDto: CreateUserDto) {
         const candidate = await this.userService.getUserByEmail(userDto.email);
         if(candidate) {
@@ -24,10 +25,19 @@ export class AuthService {
         return this.generateToken(user);
     }
 
-    async generateToken(user: User) {
+    private async generateToken(user: User) {
         const payload = {email: user.email, id: user.id, roles: user.roles};
         return {
             token: this.jwtService.sign(payload)
         };
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if(user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({message: "Некорректный логин или пароль "});
     }
 }
